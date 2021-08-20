a30a991f421ead981b8d0577addbf17fb4b3ad55
Artem Golovinskiy <thegolova@gmail.com>
2021-08-20 19:44:28 +0300

    add router

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,52 @@
+class UserController {
+  async registration(req, res, next) {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+  async login(req, res, next) {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+  async logout(req, res, next) {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+  async activate(req, res, next) {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+  async refresh(req, res, next) {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+  async getUsers(req, res, next) {
+    console.log("getUsers")
+    try {
+      res.json(['123', 'asdf'])
+    } catch (e) {
+
+    }
+  }
+}
+
+module.exports = new UserController();
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const router = require('./router/index');
 
 const PORT = process.env.PORT || 5000;
 const app = express()
@@ -11,17 +12,18 @@ const app = express()
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use('/api', router);
 
 const start = async  () =>  {
-    try {
-        await mongoose.connect(process.env.DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        app.listen(PORT, () => console.log(`server started on ${PORT}`))
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    app.listen(PORT, () => console.log(`server started on ${PORT}`))
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 start();
