ac539f97406efd36c0a7228e993093a1b60e7470
Artem Golovinskiy <thegolova@gmail.com>
2021-08-23 15:03:27 +0300

    feat: add activation account

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -29,6 +29,15 @@ class UserService {
       user: userDto
     }
   }
+
+  async activate(activationLink) {
+    const user = await UserModel.findOne({activationLink})
+    if (!user) {
+      throw new Error('некорректная ссылка активации')
+    }
+    user.isActivated = true;
+    await user.save();
+  }
 }
 
 module.exports = new UserService();
