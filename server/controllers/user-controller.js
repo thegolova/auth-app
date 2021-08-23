2ca44a4e06a93f2fde612144a2eebcef7823ac6e
Artem Golovinskiy <thegolova@gmail.com>
2021-08-23 18:48:27 +0300

    feat: added refresh

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -27,7 +27,7 @@ class UserController {
       const userData = await userService.login(email, password);
       //устанавливаем refresh token в куки
       res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
-      return res.json(userData)
+      return res.json(userData);
     } catch (e) {
         next(e);
     }
@@ -56,7 +56,11 @@ class UserController {
 
   async refresh(req, res, next) {
     try {
-
+      const {refreshToken} = req.cookies;
+      const userData = await userService.refresh(email, password);
+      //устанавливаем refresh token в куки
+      res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+      return res.json(userData);
     } catch (e) {
         next(e);
     }
